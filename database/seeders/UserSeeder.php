cdee47bce4713fd26e51d5838b3bd82cc8002f04
crimson_speedster <shvets.o@mavericks.agency>
2026-05-01 12:52:22 +0200

    included nova

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,33 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'email' => 'test@gmail.com',
+            'email' => 'developer@gmail.com',
             'password' => Hash::make('password'),
             'role' => UserRoles::Developer,
+        ]);
+
+        User::factory()->create([
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoles::Admin,
+        ]);
+
+        User::factory()->create([
+            'email' => 'content_manager@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoles::ContentManager,
+        ]);
+
+        User::factory()->create([
+            'email' => 'shop_manager@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoles::ShopManager,
+        ]);
+
+        User::factory()->create([
+            'email' => 'customer@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoles::Customer,
         ]);
 
         User::factory()->count(45)->create();
