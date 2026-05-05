657caf3b880fc2b3010b07e7ba713a01a34bb6b7
crimson_speedster <shvets.o@mavericks.agency>
2026-05-05 17:39:22 +0200

    changes from v13 to v12

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use App\Enums\UserRoles;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -14,12 +12,23 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['email', 'phone', 'password', 'role'])]
-#[Hidden(['password', 'role', 'remember_token'])]
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'email',
+        'phone',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'role',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
