2095f84e42dc92093b5a07d2d8810bf7171b79a3
crimson_speedster <shvets.o@mavericks.agency>
2026-05-26 10:37:30 +0200

    fixes for sitemap index

--- app/Console/Commands/GenerateIndexSitemap.php
+++ app/Console/Commands/GenerateIndexSitemap.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
+use Illuminate\Support\Facades\File;
 
 class GenerateIndexSitemap extends Command
 {
@@ -26,13 +28,47 @@ class GenerateIndexSitemap extends Command
      */
     public function handle()
     {
-        Sitemap::create()
-            ->add(frontend_url('/sitemaps/pages.xml'))
-            ->add(frontend_url('/sitemaps/collections.xml'))
-            ->add(frontend_url('/sitemaps/categories.xml'))
-            ->add(frontend_url('/sitemaps/filter-pages.xml'))
-            ->add(frontend_url('/sitemaps/products.xml'))
-            ->add(frontend_url('/sitemaps/shops.xml'))
+        SitemapIndex::create()
+            ->add(
+                Sitemap::create(frontend_url('/sitemaps/pages.xml'))
+                    ->setLastModificationDate(
+                        now()->setTimestamp(
+                            File::lastModified(public_path('sitemaps/pages.xml'))
+                        )
+                    )
+            )
+            ->add(
+                Sitemap::create(frontend_url('/sitemaps/products.xml'))
+                    ->setLastModificationDate(
+                        now()->setTimestamp(
+                            File::lastModified(public_path('sitemaps/products.xml'))
+                        )
+                    )
+            )
+            ->add(
+                Sitemap::create(frontend_url('/sitemaps/shops.xml'))
+                    ->setLastModificationDate(
+                        now()->setTimestamp(
+                            File::lastModified(public_path('sitemaps/shops.xml'))
+                        )
+                    )
+            )
+            ->add(
+                Sitemap::create(frontend_url('/sitemaps/categories.xml'))
+                    ->setLastModificationDate(
+                        now()->setTimestamp(
+                            File::lastModified(public_path('sitemaps/categories.xml'))
+                        )
+                    )
+            )
+            ->add(
+                Sitemap::create(frontend_url('/sitemaps/filter-pages.xml'))
+                    ->setLastModificationDate(
+                        now()->setTimestamp(
+                            File::lastModified(public_path('sitemaps/filter-pages.xml'))
+                        )
+                    )
+            )
             ->writeToFile(public_path('sitemap_index.xml'));
 
         $this->info('Generated index sitemap successfully');
