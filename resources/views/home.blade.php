c10cc52a1d921597471d240fc28be8df3ca936b3
crimson_speedster <shvets.o@mavericks.agency>
2026-08-05 09:16:25 +0200

    small ui changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,44 @@
         <meta name="robots" content="noindex, nofollow, noarchive">
         <link rel="icon" href="{{asset('favicon.ico')}}">
         <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            body {
+                font-family: Arial, sans-serif;
+            }
+
+            .container {
+                max-width: 1200px;
+                margin: 0 auto;
+                padding: 0 15px;
+            }
+
+            main {
+                min-height: 100vh;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .text-centner {
+                text-align: center;
+            }
+        </style>
     </head>
 
     <body>
-       <h1>Nothing to see here</h1>
+       <main>
+           <section class="text-centner">
+               <div class="container">
+                   <h1>Nice try =)</h1>
+               </div>
+           </section>
+       </main>
     </body>
 </html>
