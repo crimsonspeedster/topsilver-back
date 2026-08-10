9af293c2fa29dbcda549b5f423a61a00bbc610b5
crimson_speedster <shvets.o@mavericks.agency>
2026-08-10 18:21:18 +0200

    small fix for ProductSeeder

--- database/seeders/ProductionSeeder.php
+++ database/seeders/ProductionSeeder.php
@@ -14,6 +14,7 @@ class ProductionSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            RegionSeeder::class,
             CitySeeder::class,
             PaymentMethodSeeder::class,
             ShippingMethodSeeder::class,
