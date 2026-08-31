3c87ddc86fcd037f64eb67fe1e2ee2e0d1fc4456
crimson_speedster <shvets.o@mavericks.agency>
2026-08-31 06:16:40 +0200

    removed Log

--- app/Jobs/SyncMediaBatchJob.php
+++ app/Jobs/SyncMediaBatchJob.php
@@ -3,7 +3,6 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\Log;
 use Spatie\MediaLibrary\HasMedia;
 use Throwable;
 
@@ -35,12 +34,6 @@ class SyncMediaBatchJob implements ShouldQueue
 
     private function syncCollection($model, array $item): void
     {
-        Log::info('SYNC MEDIA ITEM', [
-            'id' => $item['id'],
-            'collection' => $item['collection'],
-            'urls' => $item['urls'],
-        ]);
-
         $collection = $item['collection'];
         $urls = collect($item['urls'] ?? [])
             ->map(fn ($url) => trim($url))
@@ -60,11 +53,6 @@ class SyncMediaBatchJob implements ShouldQueue
             }
 
             try {
-                Log::info('DOWNLOADING MEDIA', [
-                    'post_id' => $item['id'],
-                    'url' => $url,
-                ]);
-
                 $model
                     ->addMediaFromUrl($url)
                     ->withCustomProperties([
